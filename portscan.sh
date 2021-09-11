0a90c18356751e6d47c845479cb6ccd0b26da33e
nand-0 <fmpanizzads@gmail.com>
2021-09-11 20:05:52 -0300

    fixed udp/tcp and nmap opts

--- portscan.sh
+++ portscan.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 
+IFACE=tun0
+RATE=1000
+NMAP_OPTS="--script default,version,vuln"
+
 usage() {
 	(>&2 echo -e "$0\nScan a host with masscan and nmap")
 	(>&2 echo -e "Usage: $0 -t <TARGET IP> -o <OUTPUT FILE>")	
 }
+
 
 
 while [ $# -gt 0 ];do
@@ -19,6 +24,14 @@ while [ $# -gt 0 ];do
             OUTFILE=$2
 			shift
         ;;
+        '-i')
+            IFACE=$2
+			shift
+        ;;
+        '-r')
+            RATE=$2
+	    		shift
+	;;
 
         *)
            	usage
@@ -34,20 +47,26 @@ done
 
 printf "[*] Starting masscan on %s..\n" ${TARGET}
 
-masscan -p1-65535,U:1-65535 ${TARGET} --rate=1000 -e tun0 -oG ${OUTFILE}.masscan
+masscan -p1-65535,U:1-65535 ${TARGET} --rate=${RATE} -e ${IFACE} -oG ${OUTFILE}.masscan
 
 
+TCP_PORTS=$(grep open  ${OUTFILE}.masscan | grep tcp | awk '{print $NF}' | awk -F "/" '{print $1}' | sort -n)
+UDP_PORTS=$(grep open  ${OUTFILE}.masscan | grep udp | awk '{print $NF}' | awk -F "/" '{print $1}' | sort -n)
 
-PORTS=$(grep open ${OUTFILE}.masscan | awk '{print $NF}' | awk -F "/" '{print $1}' | sort -n)
-
-printf "[*] Found %d open ports:\n" $(echo ${PORTS} | wc -w)
+printf "[*] Found %d TCP open ports:\n" $(echo ${TCP_PORTS} | wc -w)
+printf "[*] Found %d UDP open ports:\n" $(echo ${UDP_PORTS} | wc -w)
 grep open ${OUTFILE}.masscan | awk '{print $NF}' | awk -F "/" '{print $1,$3}' | column -t | sort -n
 
-NMAP_PORTS=$(echo $PORTS | sed 's/ /,/g')
+NMAP_TCP_PORTS=$(echo $TCP_PORTS | sed 's/ /,/g')
+NMAP_UDP_PORTS=$(echo $UDP_PORTS | sed 's/ /,/g')
 
-printf "[*] Starting nmap scan on %s on ports %s..\n" ${TARGET} ${NMAP_PORTS}
-
-nmap -sC -sV -p ${NMAP_PORTS} -oA ${OUTFILE} ${TARGET}
+if [[ "X${NMAP_TCP_PORTS}" != "X" ]];then
+	printf "[*] Starting nmap TCP scan on %s on ports %s..\n" ${TARGET} ${NMAP_TCP_PORTS}
+	nmap ${NMAP_OPTS} -e ${IFACE} -p ${NMAP_TCP_PORTS} -oA "${OUTFILE}.tcp" ${TARGET}
+elif [[ "X${NMAP_UDP_PORTS}" != "X" ]];then
+	printf "[*] Starting nmap UDP scan on %s on ports %s..\n" ${TARGET} ${NMAP_UDP_PORTS}
+	nmap ${NMAP_OPTS} -e ${IFACE} -p ${NMAP_UDP_PORTS} -oA "${OUTFILE}.udp" ${TARGET}
+fi
 
 
 
